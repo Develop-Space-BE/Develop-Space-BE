e4da90056cbd223df31e1d8446c3aabb4c7df828
JeekLee <j3kkun@gmail.com>
2023-03-17 08:34:06 +0900

    feat: Question Domain Entity 생성

--- src/main/java/developspace/com/developspace/question/entity/Bookmark.java
+++ src/main/java/developspace/com/developspace/question/entity/Bookmark.java
@@ -1,4 +1,26 @@
 package developspace.com.developspace.question.entity;
 
+
+import developspace.com.developspace.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Bookmark {
+    @EmbeddedId
+    private BookmarkCompositeKey id;
+
+    @MapsId("memberId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @MapsId("questionId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Question question;
 }
---
+++ src/main/java/developspace/com/developspace/question/entity/SubCategory.java
@@ -0,0 +1,5 @@
+package developspace.com.developspace.question.entity;
+
+public enum SubCategory {
+    ATTITUDE, REACT, VUE, ANGULAR, SPRING, NODE, FASTAPI, DRF
+}
