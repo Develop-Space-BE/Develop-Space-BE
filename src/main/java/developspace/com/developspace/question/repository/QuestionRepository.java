eb49fdc0d597f48ce45fd55b153f3606b8d59df0
JeekLee <j3kkun@gmail.com>
2023-03-25 15:55:52 +0900

    feat: Question Repository findById 기능 추가

--- src/main/java/developspace/com/developspace/question/repository/QuestionRepository.java
+++ src/main/java/developspace/com/developspace/question/repository/QuestionRepository.java
@@ -4,9 +4,11 @@ import developspace.com.developspace.question.entity.Question;
 import developspace.com.developspace.question.entity.SubCategory;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface QuestionRepository {
     Question save(Question question);
+    Optional<Question> findById(Long id);
     void deleteById(Long questionId);
     List<Question> findAllBySubCategory(SubCategory subCategory);
 }
