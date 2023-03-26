5abb6f907056f5d65a351b5abac13b5ad0c96bc4
JeekLee <j3kkun@gmail.com>
2023-03-26 16:58:19 +0900

    feat: Question Controller 구현

--- src/main/java/developspace/com/developspace/common/response/success/SuccessCode.java
+++ src/main/java/developspace/com/developspace/common/response/success/SuccessCode.java
@@ -17,7 +17,13 @@ public enum SuccessCode {
     LIKE_ANSWER(HttpStatus.OK, "답변 좋아요 성공", 2000),
     UNLIKE_ANSWER(HttpStatus.OK, "답변 좋아요 취소 성공", 2000),
     GET_ANSWER(HttpStatus.OK, "답변 조회 성공", 2000),
-    MY_ANSWER(HttpStatus.OK, "내 답변 조회 성공", 2000);
+    MY_ANSWER(HttpStatus.OK, "내 답변 조회 성공", 2000),
+    WRITE_QUESTION(HttpStatus.OK, "질문 작성 성공", 2000),
+    GET_QUESTION_BY_SUBCATEGORY(HttpStatus.OK, "소분류에 따른 질문 조회 성공", 2000),
+    UPDATE_QUESTION(HttpStatus.OK, "질문 수정 성공", 2000),
+    DELETE_QUESTION(HttpStatus.OK, "질문 삭제 성공", 2000),
+    LIKE_QUESTION(HttpStatus.OK, "질문 좋아요 성공", 2000),
+    BOOKMARK_QUESTION(HttpStatus.OK, "질문 북마크 성공", 2000);
 
 
     private final HttpStatus httpStatus;
