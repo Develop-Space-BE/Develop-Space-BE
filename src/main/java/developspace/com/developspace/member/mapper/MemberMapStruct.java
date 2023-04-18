5f0acee0615be87a02fc6e8322053e521424cbfb
jinhyoje <jjjhhh810@naver.com>
2023-04-18 23:32:03 +0900

    feat : 멤버 정보 조회 API 구현

--- src/main/java/developspace/com/developspace/member/mapper/MemberMapStruct.java
+++ src/main/java/developspace/com/developspace/member/mapper/MemberMapStruct.java
@@ -1,5 +1,8 @@
 package developspace.com.developspace.member.mapper;
 
+import developspace.com.developspace.answer.dto.AnswerListDto;
+import developspace.com.developspace.answer.entity.Answer;
+import developspace.com.developspace.member.dto.KakaoMemberDto;
 import developspace.com.developspace.member.dto.RequestSignupDto;
 import developspace.com.developspace.member.entity.Member;
 import org.mapstruct.Mapper;
@@ -10,4 +13,13 @@ public interface MemberMapStruct {
     MemberMapStruct MEMBER_MAPPER = Mappers.getMapper(MemberMapStruct.class);
 
     Member RequestSignupDtotoMember(RequestSignupDto requestSignupDto);
+    default KakaoMemberDto memberDto(Member member) {
+        return KakaoMemberDto.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .profileImg(member.getProfileImg())
+                .build();
+
+    }
 }
